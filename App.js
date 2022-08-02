01de0968fdb126191dcfdaf5d8e6e77c2f8cadd9
Lê Hồng Long <70050446+lehonglong2411@users.noreply.github.com>
2022-08-02 19:52:08 +0700

    20:57

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+native';
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <Text>Hello world 3</Text>
       
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
