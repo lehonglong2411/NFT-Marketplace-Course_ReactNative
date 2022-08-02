fb870970182fb89317d7fdb24af1c366f55b6f74
Le Hong Long <lehonglong2411@gmail.com>
2022-08-02 20:10:51 +0700

    Long day

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-
+import { createStackNavigator } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFont } from './screen';
+
 
 export default function App() {
   return (
