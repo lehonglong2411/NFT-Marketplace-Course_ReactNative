e505b7825c5f72552ac06d20554460a8ba2560b9
Lê Hồng Long <110602572+HongHai24@users.noreply.github.com>
2022-08-04 18:35:25 +0700

    30:00 8/4/2022 6:35PM

--- App.js
+++ App.js
@@ -13,6 +13,13 @@ const theme = {
 }
 
 const App =() => {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{ headerShown: false}} initialRouteName="Home">
