0b13e3243c5146b94390a68abb9b501ff5c92392
Le Hong Long <lehonglong2411@gmail.com>
2022-08-06 22:55:20 +0700

    Project

--- screens/Home.js
+++ screens/Home.js
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import React from 'react'
 import { View, SafeArea, FlatList,Text } from 'react-native';
 import { COLORS, NFTData } from '../constants';
-import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
+
 
 const Home = () => {
   return (
     <SafeAreaView style = { {flex: 1}}>
-    
+
     </SafeAreaView>
   )
 }
