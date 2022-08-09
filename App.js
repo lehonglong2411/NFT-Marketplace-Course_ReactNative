73aa9c4d2641835d48313e6a4aa42e8f0fca5ee9
Lê Hồng Long <70050446+lehonglong2411@users.noreply.github.com>
2022-08-09 20:31:56 +0700

    before merge from macos

--- App.js
+++ App.js
@@ -4,15 +4,33 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useFont } from './screen';
 
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <StackNavigator>
+const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme,
+  color: {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: "transparent"
 
-      </StackNavigator>
+    }
+  }
+}
+
+const App = () => {
+  return (
+    <NavigationContainer theme={theme} >
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName = "Home">
+        <Stack.Screen name = "Home" component = {Home} />
+
+        <Stack.Screen name = ""/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
