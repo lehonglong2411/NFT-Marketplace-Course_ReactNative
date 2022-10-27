cf30acf78fba69d1e87ed4f8673aaa372793d2f1
Hong Long <lehonglong2411@gmail.com>
2022-10-27 13:58:03 +0700

    43:41

--- screens/Home.js
+++ screens/Home.js
@@ -2,12 +2,37 @@ import { useState } from 'react';
 import React from 'react'
 import { View, SafeAreaView, FlatList,Text } from 'react-native';
 import { COLORS, NFTData } from '../constants';
-import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
+
+import FocusedStatusBar from  '../components/FocusedStatusBar';
+import HomeHeader from '../components/HomeHeader';
 
 const Home = () => {
   return (
     <SafeAreaView style = { {flex: 1}}>
-      {/* <FocusedStatusBar background = {COLORS.primary} /> */}
+     <FocusedStatusBar/>
+
+     <View style = {{flex :1}}>
+        <View style = {{ zIndex: 0}}>
+          <FlatList
+            data = {NFTData}
+            renderItem = {({item} ) => <Text>{item.name}</Text>}
+            keyExtractor = {(item) => item.id}
+            showsVerticalScrollIndicator = {false}
+            ListHeaderComponent = {<HomeHeader/>}
+           />
+        </View>
+
+        <View style = {{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          
+        }}>
+
+        </View>
+     </View>
     </SafeAreaView>
   )
 }
