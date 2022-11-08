dbe219753c57082e2556a0fa27d66c8f36d5a1c3
Hong Long <lehonglong2411@gmail.com>
2022-11-08 18:39:31 +0700

    interative nft card

--- components/Button.js
+++ components/Button.js
@@ -31,11 +31,30 @@ export const CircleButton = ( {imgUrl, handlePress, ...props}) => {
 }
 
 
-export const RectButton = () => {
+export const RectButton = ({imgUrl, handlePress, ...props}) => {
     return (
-      <View>
-        <Text>Button</Text>
-      </View>
+      <TouchableOpacity 
+        style = {{ 
+            width: 40,
+            height: 40,
+           backgroundColor: COLORS.white,
+           position: 'absolute',
+           borderRadius: SIZES.extraLarge,
+           alignItems: 'center',
+           justifyContent: 'center',
+           ...SHADOWS.light,
+           ...props,
+           
+        }}
+        onPress={handlePress}
+    >
+        <Image 
+            source = {imgUrl}
+            resizeMode = "contain"
+            style = {{ position: "absolute", width: 24, height: 24, bottom: 0, right: 0}}
+        />
+        <Text>Place a bid</Text>
+    </TouchableOpacity>
     )
   }
 
